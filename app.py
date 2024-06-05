b43bfe9dfcd6350668451de0ebfc04342c6ed7a4
ICE_InDa_MIXER <104935198+sayan-does@users.noreply.github.com>
2024-06-05 19:20:28 +0530

    new commit

--- app.py
+++ app.py
@@ -3,60 +3,75 @@ from PIL import Image
 import torch
 import numpy as np
 from utility import preprocess_image, load_model, generate_gradcam
-import base64
 
 # Streamlit app
 st.set_page_config(page_title="Check a Meme",
                    page_icon=":smile:", layout="wide")
 
-# Function to set the background image
+# Apply custom CSS for a background image and other styles
 
 
-def set_background(png_file):
-    with open(png_file, 'rb') as f:
-        data = f.read()
-    bin_str = base64.b64encode(data).decode()
-    page_bg_img = f'''
-    <style>
-    .stApp {{
-        background-image: url("data:image/png;base64,{bin_str}");
-        background-size: cover;
-    }}
-    </style>
-    '''
-    st.markdown(page_bg_img, unsafe_allow_html=True)
+# def add_custom_css():
+#     st.markdown(
+#         """
+#         <style>
+#         .stApp {
+#             background-image: url("G:\Projectsdocs\memes\csspatternbymagicpattern.jpeg");
+#             background-size: cover;
+#             color: white;
+#         }
 
+#         .prediction {
+#             font-size: 24px;
+#             font-weight: bold;
+#         }
 
-# Replace 'background.png' with the path to your background image file
-set_background('css-pattern-by-magicpattern (3).png')
+#         .prediction.not-meme {
+#             color: #8B0000;
+#         }
 
-# Function to set custom header font
+#         .image-container {
+#             text-align: center;
+#             margin-bottom: 20px;
+#         }
 
+#         .image-container img {
+#             max-width: 80%;  /* Adjust this value to reduce the image size */
+#             height: auto;
+#         }
+#         </style>
+#         """,
+#         unsafe_allow_html=True
+#     )
 
-def set_header_font_and_border():
+def add_custom_css():
     st.markdown(
         """
         <style>
-        @import url('https://fonts.googleapis.com/css2?family=Playfair+Display:wght@700&display=swap');
-        .stApp h1 {
-            font-family: 'Playfair Display', serif;
-            font-size: 3em;
-            color: #00000e;
+        .stApp {
+            background-color: #272728;
+            opacity: 0.8;
+            background-image:  repeating-radial-gradient( circle at 0 0, transparent 0, #272728 36px ), repeating-linear-gradient( #1c82a155, #1c82a1 );
+            color: white;
+        }
+
+        .prediction {
+            font-size: 24px;
             font-weight: bold;
         }
-        .stApp h3 {
 
-            color: #00000e;
-            font-size: 1.5em;
-            font-weight: bold;
-            
+        .prediction.not-meme {
+            color: #8B0000;
         }
-        .stApp p {
 
-            color: #00000e;
-            font-size: 1em;
-            font-weight: bold;
-            
+        .image-container {
+            text-align: center;
+            margin-bottom: 20px;
+        }
+
+        .image-container img {
+            max-width: 80%;  /* Adjust this value to reduce the image size */
+            height: auto;
         }
         </style>
         """,
@@ -64,62 +79,77 @@ def set_header_font_and_border():
     )
 
 
-# Set custom header font
-set_header_font_and_border()
+add_custom_css()
+
+
+
+add_custom_css()
 
 st.title("Check a Meme")
-st.write("### What is a Meme?")
-st.write("""
-Memes are humorous images, videos, text, etc. A meme is a virally transmitted image embellished with text,
-usually sharing pointed commentary on cultural symbols, social ideas, or current events.
-If you have a slow meme brain, lend my model to know a image is a meme or not!
-""")
+st.subheader(
+    "Memes are humorous images, videos, text, etc. A meme is a virally transmitted image embellished with text, usually sharing pointed commentary on cultural symbols, social ideas, or current events. If you have a slow meme brain, lend my model to know a image is a meme or not!")
 
-col1, col2 = st.columns(2)
+# Column layout for file uploader and results
+col1, col2 = st.columns([1, 2])
 
 with col1:
-    uploaded_file = st.file_uploader("Choose an image...", type=[
+    uploaded_file = st.file_uploader("Drag and Drop image", type=[
                                      "jpg", "jpeg", "png", "webp"])
 
 if uploaded_file is not None:
+    # Load and preprocess image
+    image = Image.open(uploaded_file).convert('RGB')
+    image_tensor = preprocess_image(uploaded_file)
+
+    # Device setup
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    # Load model
+    model = load_model('best_model.pth', device)
+
+    # Predict the class
+    model.eval()
+    output = model(image_tensor.to(device))
+    _, predicted = torch.max(output, 1)
+    class_idx = predicted.item()
+    label = 'Meme' if class_idx == 0 else 'Not Meme'
+
+    # Generate Grad-CAM visualization
+    target_layer = model.features.denseblock4
+    gradcam_img = generate_gradcam(model, image_tensor, device, target_layer)
+
+    # Convert Grad-CAM and original image to displayable format
+    gradcam_img_pil = Image.fromarray((gradcam_img * 255).astype(np.uint8))
+
+    # Display the prediction
+    with col1:
+        if label == 'Meme':
+            st.markdown(
+                f'<p class="prediction">Prediction: {label} ✅</p>', unsafe_allow_html=True)
+        else:
+            st.markdown(
+                f'<p class="prediction not-meme">Prediction: {label}</p>', unsafe_allow_html=True)
+
+        # Display Grad-CAM color explanation
+        st.markdown("""
+        **Grad-CAM Color Explanation:**
+
+        <div style="display: flex; align-items: center;">
+            <div style="width: 20px; height: 20px; background-color: red; margin-right: 5px;"></div> Red: High importance regions (strong influence on the prediction)
+        </div>
+        <div style="display: flex; align-items: center;">
+            <div style="width: 20px; height: 20px; background-color: orange; margin-right: 5px;"></div> Yellow/Orange: Moderate importance regions
+        </div>
+        <div style="display: flex; align-items: center;">
+            <div style="width: 20px; height: 20px; background-color: blue; margin-right: 5px;"></div> Blue/Green: Low importance regions (weak influence on the prediction)
+        </div>
+        """, unsafe_allow_html=True)
+
     with col2:
-        image = Image.open(uploaded_file).convert('RGB')
-        st.image(image, caption='Uploaded Image', use_column_width=True)
-        st.write("Classifying...")
-
-        # Device setup
-        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-
-        # Load model
-        model = load_model('best_model.pt', device)
-
-        # Preprocess image
-        image_tensor = preprocess_image(uploaded_file)
-
-        # Predict the class
-        model.eval()
-        output = model(image_tensor.to(device))
-        _, predicted = torch.max(output, 1)
-        class_idx = predicted.item()
-        label = 'Meme' if class_idx == 0 else 'Not Meme'
-
-        # Display the prediction
-        st.markdown(f"**Prediction:** {label}")
-
-        # Generate Grad-CAM visualization
-        target_layer = model.features.denseblock4
-        gradcam_img = generate_gradcam(
-            model, image_tensor, device, target_layer)
-
-        # Convert Grad-CAM and original image to displayable format
-        original_img = Image.open(uploaded_file).convert('RGB')
-        gradcam_img_pil = Image.fromarray((gradcam_img * 255).astype(np.uint8))
-
-        # Display the images side by side
-        col3, col4 = st.columns(2)
-        with col3:
-            st.image(original_img, caption='Original Image',
-                     use_column_width=True)
-        with col4:
-            st.image(gradcam_img_pil, caption='Grad-CAM heatmap of image features responsible for the prediction',
-                     use_column_width=True)
+        st.markdown('<div class="image-container">', unsafe_allow_html=True)
+        st.image(image, caption='Original Image', use_column_width=True)
+        st.markdown('</div>', unsafe_allow_html=True)
+        st.markdown('<div class="image-container">', unsafe_allow_html=True)
+        st.image(gradcam_img_pil,
+                 caption='Grad-CAM heatmap of image features responsible for the prediction', use_column_width=True)
+        st.markdown('</div>', unsafe_allow_html=True)
